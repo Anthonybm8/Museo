ee153339827207358bb791fb279c6c6b65c90881
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-06-04 16:08:37 -0500

    se hizo las migraciones

--- Aplicaciones/Artista/models.py
+++ Aplicaciones/Artista/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from Aplicaciones.Obra.models import Obra
 
 # Create your models here.
+class Artista(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    Obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='artistas', null=True, blank=True)
+    biografia = models.FileField(upload_to='artistas', null=True, blank=True)
--- Aplicaciones/Obra/models.py
+++ Aplicaciones/Obra/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Obra(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateField()
+    foto = models.ImageField(upload_to='obras', null=True, blank=True)
+    informacion = models.FileField(upload_to='obras', null=True, blank=True)
