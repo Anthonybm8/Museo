fe2cc023a544bb8d7efbdd8f869096b797bb75fa
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-06-05 14:38:20 -0500

    se agrgo la edicion de artista

--- Aplicaciones/Artista/urls.py
+++ Aplicaciones/Artista/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('nuevoArtista',views.nuevoArtista,name='nuevoArtista'),
     path('guardarArtista',views.guardarArtista,name='guardarArtista'),
     path('eliminarArtista/<id>', views.eliminarArtista, name='eliminarArtista'),
+    path('editarArtista/<id>', views.editarArtista, name='editarArtista'),
+    path('procesarEdicionArtista/<id>', views.procesarEdicionArtista, name='procesarEdicionArtista'),
 ]
--- Aplicaciones/Artista/views.py
+++ Aplicaciones/Artista/views.py
@@ -42,3 +42,41 @@ def eliminarArtista(request, id):
         artista.delete()
         messages.success(request, "Artista eliminado exitosamente")
         return redirect('inicio')
+
+def editarArtista(request, id):
+    artista = Artista.objects.get(id=id)
+    obras = Obra.objects.all()  # Obtener todas las obras disponibles
+    return render(request, "editarArtista.html", {
+        'artista': artista,
+        'obras': obras  # Pasar las obras al template
+    })
+
+def procesarEdicionArtista(request, id):
+    artista = Artista.objects.get(id=id)
+    
+    if request.method == 'POST':
+        # Procesar el formulario
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        obra_ids = request.POST.getlist("obra")
+
+        foto = request.FILES.get('foto')
+        biografia = request.FILES.get('biografia')
+
+        # Actualizar los campos del artista
+        artista.nombre = nombre
+        artista.apellido = apellido
+        artista.foto = foto if foto else artista.foto  # Mantener la foto actual si no se sube una nueva
+        artista.biografia = biografia if biografia else artista.biografia  # Mantener la biografía actual
+
+        artista.save()
+
+        # Actualizar las obras asociadas
+        artista.obra.clear()  # Limpiar las obras actuales
+        for id in obra_ids:
+            if id:  # Solo si id no está vacío
+                obra = Obra.objects.get(id=id)
+                artista.obra.add(obra)
+
+        messages.success(request, "Artista editado exitosamente")
+        return redirect('inicio')
