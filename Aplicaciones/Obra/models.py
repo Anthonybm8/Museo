ab35f477d0f3b11666de042d7d781f7cafb24214
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-06-05 16:47:38 -0500

    se agrego los campos a models y a editar obra

--- Aplicaciones/Obra/models.py
+++ Aplicaciones/Obra/models.py
@@ -7,3 +7,6 @@ class Obra(models.Model):
     fecha_creacion = models.DateField()
     foto = models.ImageField(upload_to='obras', null=True, blank=True)
     informacion = models.FileField(upload_to='obras', null=True, blank=True)
+    tecnica_utilizada = models.CharField(max_length=100, default="Técnica desconocida")
+    anio_creacion = models.IntegerField(null=True, blank=True)
+    imagen_obra = models.ImageField(upload_to='obras/', null=True, blank=True)
--- Museo/settings.py
+++ Museo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Museo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'dbactualizado.sqlite3',
+        'NAME': BASE_DIR / 'dbaFINALYTI.sqlite3',
     }
 }
 
